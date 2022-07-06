6b9a0ad6c108bb062c3255ca59d33ef7c9191526
Marcio <larramendimarcio@gmail.com>
2022-07-05 23:01:10 -0300

    -Se completó la primer entrega siguiendo los pasos del pdf.

--- Usuarios/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Para el futuro
-# class Usuario(models.Model):
-#     usuario = models.IntegerField()
-#     nombre_completo = models.CharField(max_length=50)
-#     fecha_nacimiento = models.DateField()
--- Videojuegos/views.py
+++ Videojuegos/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Videojuegos.models import Videojuego
+from .forms import FormVideojuego, FormBusquedaVideojuego
 
 
 def inicio(request):
-    videojuego_1 = Videojuego(nombre='Camellosaurio Ninja', fecha_lanzamiento='26/06/2021',genero='Aventura',valoracion=98)
-    return render(request, 'index.html', {'videojuego':videojuego_1})
+    return render(request, 'index.html')
+
+
+def acerca_de_nosotros(request):
+    return render(request, 'about.html')
+
+
+def crear_videojuego(request):
+    form = FormVideojuego(request.POST)
+
+    if form.is_valid():
+        data = form.cleaned_data
+
+        videojuego = Videojuego(
+            nombre=data.get('nombre'),
+            fecha_lanzamiento=data.get('fecha_lanzamiento'),
+            genero=data.get('genero'),
+            valoracion=data.get('valoracion')
+        )
+        videojuego.save()
+
+        return redirect('listado_videojuegos')
+
+    else:
+        return render(request, 'crear_videojuego.html', {'form': form})
+
+
+def listado_videojuegos(request):
+    nombre_de_busqueda = request.GET.get('nombre')
+
+    if nombre_de_busqueda:
+        listado_videojuegos = Videojuego.objects.filter(nombre__icontains=nombre_de_busqueda)
+    else:
+        listado_videojuegos = Videojuego.objects.all()
+
+    form = FormBusquedaVideojuego()
+    return render(request, 'listado_videojuegos.html', {'listado_videojuegos': listado_videojuegos, 'form': form})
