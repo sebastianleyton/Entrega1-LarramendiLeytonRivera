a497b8d3f46351d988fefbf5a106ee47ca358b59
Juan Manuel <jmr092@gmail.com>
2022-07-05 23:18:16 -0300

    - Agregue placeholder para mostrar el formato de la fecha correcto

--- Videojuegos/forms.py
+++ Videojuegos/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 class FormVideojuego(forms.Form):
     nombre = forms.CharField(max_length=50)
-    fecha_lanzamiento = forms.DateField()
+    fecha_lanzamiento = forms.DateField(
+            widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'})
+        )
     genero = forms.CharField(max_length=50)
     valoracion = forms.IntegerField()
 
