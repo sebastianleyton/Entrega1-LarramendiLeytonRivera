e8a71019369a1ec3d202ef1b5d0b1edd523acf02
Marcio <larramendimarcio@gmail.com>
2022-07-05 23:04:09 -0300

    -Se añadió forms.py

---
+++ Videojuegos/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class FormVideojuego(forms.Form):
+    nombre = forms.CharField(max_length=50)
+    fecha_lanzamiento = forms.DateField()
+    genero = forms.CharField(max_length=50)
+    valoracion = forms.IntegerField()
+
+class FormBusquedaVideojuego(forms.Form):
+    nombre = forms.CharField(max_length=50)
