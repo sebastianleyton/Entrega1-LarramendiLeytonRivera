5173286cde7bc92d0a29fce5b6fb77ce8d34756a
Juan Manuel <jmr092@gmail.com>
2022-08-01 21:26:43 -0300

    - Generada view de log out y funcionalidad

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout')
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as django_login
 
 # Create your views here.
 
 
 def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                django_login(request, user)
+                return render(request, 'index.html', {})
+            else:
+                return render(request, 'accounts/login.html', {'form': form})
+        else:
+            return render(request, 'accounts/login.html', {'form': form})
 
     form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
