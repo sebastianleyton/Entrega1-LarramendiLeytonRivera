3884213fcf5a150846e718f533291bd179a4b115
SebaGenetic <seba.powa@gmail.com>
2022-08-02 17:55:20 -0300

    - Agregado user-control al form de creación de usuario

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 
 
 class MyUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     username = forms.CharField(label='Usuario', max_length=30)
     email = forms.EmailField()
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
