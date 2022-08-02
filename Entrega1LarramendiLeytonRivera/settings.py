d6ae6701eea1f1c59366dbf6c87bac06fe916f5d
marciolarra <larramendimarcio@gmail.com>
2022-08-02 16:45:25 -0300

    -settings.py modificado

--- Entrega1LarramendiLeytonRivera/settings.py
+++ Entrega1LarramendiLeytonRivera/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
